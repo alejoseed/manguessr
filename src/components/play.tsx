5c05c55331259a3addd49fe057eb4c4793d72c7f
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2022-11-21 18:13:54 -0500

    Play added a changing button

--- src/components/play.tsx
+++ src/components/play.tsx
@@ -1,27 +1,47 @@
-import { randomBytes } from "crypto";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  useState,
+} from "react";
 import NavBar from "./navBar";
 
-function GetColor(){
-    let r,g,b
-    
-    r = Math.random() % 225
-    console.log("Hello")
-    return r
+function GetRandomColor() {
+  let color = "#";
+  // Get a rnadom number between 41 and 90
+  let r = Math.floor(Math.random() * (90 - 41 + 1) + 41).toString(16);
+  let g = Math.floor(Math.random() * (90 - 41 + 1) + 41).toString(16);
+  let b = Math.floor(Math.random() * (90 - 41 + 1) + 41).toString(16);
+  color += r;
+  color += g;
+  color += b;
+  return color;
 }
-// This function will get a random panel and assign it a number. Maybe?
-function Panels(){
 
-}
-function PlayGame () {
-    return(
+function PlayGame() {
+  const [color, setColor] = useState("");
+  return (
+    <div>
+      <NavBar />
+      <div className="flex flex-col justify-center items-center h-screen">
+        This is a fun game
         <div>
-            <NavBar/>
-            <div className="flex flex-col justify-center items-center h-screen">
-                This is a fun game
-                <div className="bg-red-500 w-20 h-20">{GetColor()}</div>
+          <div>
+            <div className="flex h-52 w-52 flex-col justify-between items-center">
+              <button
+                className="rounded-md bg-blue-400"
+                onClick={() => setColor(GetRandomColor)}
+              >Click to change the icon</button>
+              <div
+                className="h-40 w-40"
+                style={{ backgroundColor: color }}
+              ></div>
             </div>
-        </div> 
-    );
-}  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default PlayGame
+export default PlayGame;
--- src/error-page.tsx
+++ src/error-page.tsx
@@ -3,6 +3,12 @@ import { useRouteError } from "react-router-dom";
 import NavBar from "./components/navBar";
 import raw from "./raw.png";
 
+interface Error {
+  statusText?: string;
+  status?: number;
+  code?: number;
+}
+
 export default function ErrorPage() {
     const error = useRouteError() as Error;    
     console.log(error)
@@ -15,8 +21,8 @@ export default function ErrorPage() {
                     <img src={raw} alt="raw" className="w-1/2 rounded-xl shadow" />
                 </div>
                 <div className="flex flex-col items-center justify-center m-3">
-                    <h1>Oh Oh!</h1>
-                    <p>Error {error.status} {error.statusText} - Seems like you're lost. Try going back to the home page.</p>
+                    <h1>Oh Oh! - Seems like you're lost. Try going back to the home page.</h1>
+                    <p>Error {error.status} {error.statusText}</p>
                 </div>
             </div>
         </div>
