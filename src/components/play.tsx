442de0bcf60f48d7643f7c9bceea197661f9c1ad
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2022-11-22 13:32:09 -0500

    fixed the highscore and added translate to buttons

--- src/components/play.tsx
+++ src/components/play.tsx
@@ -22,12 +22,18 @@ function randomNumber() {
   return number[random];
 }
 
-function handleWinner(a: number, b: number, score : number, setScore : any, highScore : number, setHighScore : any, setAllColors: any, isClicked : any, setWinner: any) {
+function handleWinner(a: number, b: number, score: number,setScore: any,
+  highScore: number, setHighScore: any, setAllColors: any, isClicked: any,
+  setWinner: any
+) {
+
+  if (score > highScore) {
+    console.log(score, highScore);
+    setHighScore(score);
+  }
+  
   if (a === b) {
     setScore(score + 1);
-    if (score > highScore) {
-      setHighScore(score);
-    }
     handleClick(setAllColors, isClicked, setWinner);
     return "correct";
   } else {
@@ -37,7 +43,7 @@ function handleWinner(a: number, b: number, score : number, setScore : any, high
   }
 }
 
-function handleClick(setAllColors: any, isClicked : any, setWinner: any) {
+function handleClick(setAllColors: any, isClicked: any, setWinner: any) {
   let color = GetRandomColor();
   setAllColors({
     colorOne: color[0],
@@ -70,38 +76,95 @@ function PlayGame() {
         This is a fun game
         <button
           className="rounded-md bg-blue-400"
-          onClick={() =>  handleClick(setAllColors, isClicked, setWinner)}
+          onClick={() => handleClick(setAllColors, isClicked, setWinner)}
         >
           Click to change the color
         </button>
         {clicked && (
           <div>
-            <p>Select a button </p> <p>Score: {score}</p> <p>Highscore: {highScore}</p>
-            <div className="flex flex-row space-x-3 pt-5">
+            <p>Select a button </p> <p>Score: {score}</p>{" "}
+            <p>Highscore: {highScore}</p>
+            <div className="flex flex-row space-x-3 pt-3">
               <button
-                className="h-12 w-12"
-                onClick={() => setResult(handleWinner(1, winner, score, setScore, highScore, setHighScore, setAllColors, isClicked, setWinner))}
+                className="h-12 w-12 rounded-md shadow-mdhover:duration-100 hover:-translate-y-3"
+                onClick={() =>
+                  setResult(
+                    handleWinner(
+                      1,
+                      winner,
+                      score,
+                      setScore,
+                      highScore,
+                      setHighScore,
+                      setAllColors,
+                      isClicked,
+                      setWinner
+                    )
+                  )
+                }
                 style={{ backgroundColor: allColors.colorOne }}
               >
                 1
               </button>
               <button
-                className="h-12 w-12"
-                onClick={() => setResult(handleWinner(2, winner, score, setScore, highScore, setHighScore, setAllColors, isClicked, setWinner))}
+                className="h-12 w-12 rounded-md shadow-md hover:duration-100 hover:-translate-y-3"
+                onClick={() =>
+                  setResult(
+                    handleWinner(
+                      2,
+                      winner,
+                      score,
+                      setScore,
+                      highScore,
+                      setHighScore,
+                      setAllColors,
+                      isClicked,
+                      setWinner
+                    )
+                  )
+                }
                 style={{ backgroundColor: allColors.colorTwo }}
               >
                 2
               </button>
               <button
-                className="h-12 w-12"
-                onClick={() => setResult(handleWinner(3, winner, score, setScore, highScore, setHighScore, setAllColors, isClicked, setWinner))}
+                className="h-12 w-12 rounded-md shadow-md hover:duration-100 hover:-translate-y-3"
+                onClick={() =>
+                  setResult(
+                    handleWinner(
+                      3,
+                      winner,
+                      score,
+                      setScore,
+                      highScore,
+                      setHighScore,
+                      setAllColors,
+                      isClicked,
+                      setWinner
+                    )
+                  )
+                }
                 style={{ backgroundColor: allColors.colorThree }}
               >
                 3
               </button>
               <button
-                className="h-12 w-12"
-                onClick={() => setResult(handleWinner(4, winner, score, setScore, highScore, setHighScore, setAllColors, isClicked, setWinner))}
+                className="h-12 w-12 rounded-md shadow-md hover:duration-100 hover:-translate-y-3" 
+                onClick={() =>
+                  setResult(
+                    handleWinner(
+                      4,
+                      winner,
+                      score,
+                      setScore,
+                      highScore,
+                      setHighScore,
+                      setAllColors,
+                      isClicked,
+                      setWinner
+                    )
+                  )
+                }
                 style={{ backgroundColor: allColors.colorFour }}
               >
                 4
